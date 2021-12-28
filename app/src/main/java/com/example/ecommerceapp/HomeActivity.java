5711f880cc1bd224956e75d24de098a5f729ce75
nguyendienlam351 <nguyendienlam351@gmail.com>
2021-12-28 17:06:28 +0700

    admin panel

--- app/src/main/java/com/example/ecommerceapp/HomeActivity.java
+++ app/src/main/java/com/example/ecommerceapp/HomeActivity.java
@@ -2,7 +2,14 @@ package com.example.ecommerceapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.ecommerceapp.Prevalent.Prevalent;
+
+import io.paperdb.Paper;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +17,18 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Button button = (Button) findViewById(R.id.logout_btn);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Paper.init(HomeActivity.this);
+                Paper.book().destroy();
+
+                Intent intent = new Intent(HomeActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/ecommerceapp/LoginActivity.java
+++ app/src/main/java/com/example/ecommerceapp/LoginActivity.java
@@ -11,6 +11,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.ecommerceapp.Model.Users;
@@ -27,6 +28,10 @@ public class LoginActivity extends AppCompatActivity {
     private EditText inputPhoneNumber, inputPassword;
     private Button loginButton;
     private com.rey.material.widget.CheckBox chkboxRemeberMe;
+    private TextView adminLink, notAdminLink;
+
+    private String panelDbName = "Users";
+
     private ProgressDialog loadingBar;
 
     @Override
@@ -37,6 +42,8 @@ public class LoginActivity extends AppCompatActivity {
         inputPhoneNumber = (EditText) findViewById(R.id.login_phone_number_input);
         inputPassword = (EditText) findViewById(R.id.login_password_input);
         loginButton = (Button) findViewById(R.id.login_btn);
+        adminLink = (TextView) findViewById(R.id.admin_panel_link);
+        notAdminLink = (TextView) findViewById(R.id.not_admin_panel_link);
         chkboxRemeberMe = (com.rey.material.widget.CheckBox) findViewById(R.id.remember_me_chkb);
 
         Paper.init(this);
@@ -47,6 +54,26 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 loginUser();
+            }
+        });
+
+        adminLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loginButton.setText("Login Admin");
+                adminLink.setVisibility(View.INVISIBLE);
+                notAdminLink.setVisibility(View.VISIBLE);
+                panelDbName = "Admins";
+            }
+        });
+
+        notAdminLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loginButton.setText("Login");
+                adminLink.setVisibility(View.VISIBLE);
+                notAdminLink.setVisibility(View.INVISIBLE);
+                panelDbName = "Users";
             }
         });
     }
@@ -71,7 +98,7 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void allowAccessAccount(String phone, String password) {
-        if(chkboxRemeberMe.isChecked()){
+        if (chkboxRemeberMe.isChecked()) {
             Paper.book().write(Prevalent.UserPhoneKey, phone);
             Paper.book().write(Prevalent.UserPasswordKey, password);
 
@@ -84,21 +111,31 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 //check phone number exists
-                if(snapshot.child("Users").child(phone).exists()){
-                    Users usersData = snapshot.child("Users").child(phone).getValue(Users.class);
+                if (snapshot.child(panelDbName).child(phone).exists()) {
+                    Users usersData = snapshot.child(panelDbName).child(phone).getValue(Users.class);
 
-                    if(usersData.getPhone().equals(phone)){
-                        if(usersData.getPassword().equals(password)){
-                            Toast.makeText(LoginActivity.this,
-                                    "Logged in successfully.",
-                                    Toast.LENGTH_SHORT).show();
+                    if (usersData.getPhone().equals(phone)) {
+                        if (usersData.getPassword().equals(password)) {
+                            if (panelDbName.equals("Admins")) {
+                                Toast.makeText(LoginActivity.this,
+                                        "Welcom admin, you are logged in successfully.",
+                                        Toast.LENGTH_SHORT).show();
 
-                            loadingBar.dismiss();
+                                loadingBar.dismiss();
 
-                            Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
-                            startActivity(intent);
-                        }
-                        else {
+                                Intent intent = new Intent(LoginActivity.this, AdminAddNewProductActivity.class);
+                                startActivity(intent);
+                            } else if (panelDbName.equals("Users")) {
+                                Toast.makeText(LoginActivity.this,
+                                        "Logged in successfully.",
+                                        Toast.LENGTH_SHORT).show();
+
+                                loadingBar.dismiss();
+
+                                Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                                startActivity(intent);
+                            }
+                        } else {
                             Toast.makeText(LoginActivity.this,
                                     "Password is incorrect..",
                                     Toast.LENGTH_SHORT).show();
